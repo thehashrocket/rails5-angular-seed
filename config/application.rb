4eae8989d3c53cf59adcd5aa6c951939908cceeb
YOUR NAME <jasshultz@gmail.com>
2016-10-09 15:49:11 -0600

    doing things, to make it API only mode.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RoomlyRailsAngular
   class Application < Rails::Application
+    config.api_only = true # API Mode Baby!
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
